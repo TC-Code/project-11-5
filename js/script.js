2d40be90ae2d333a38d6cfd324d35eac716fb61f
Tomasz Chowaniec <35344311+TC-Code@users.noreply.github.com>
2018-03-25 11:09:51 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       var self = this;
       this.$element = $("<button>");
       this.$element.text(this.text);
-      this.$element.click(function() {
+      this.$element.on("click", function() {
         alert(self.text);
       });
       $("body").append(this.$element);
